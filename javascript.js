b76a972b2d60f08626a1ce62f48b36876ce554e2
Mona <66236313+xelinx@users.noreply.github.com>
2020-06-27 23:32:39 -0700

    update

---
+++ javascript.js
@@ -0,0 +1,6 @@
+
+  var passLength = form ("How many characters do you want in your password?")
+  var lowercase = prompt ("Do you want to include lowercase letters?")
+  var uppercase = prompt ("Do you want to include uppercase letters?")
+  var numbers = prompt ("Do you want to include numbers?")
+  var special = prompt ("Do you want to include special characters?")
