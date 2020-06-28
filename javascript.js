0cc3d4e51bcc7039c62e7d126c8c6ee0c9e11457
Mona <66236313+xelinx@users.noreply.github.com>
2020-06-28 16:53:14 -0700

    add

--- javascript.js
+++ javascript.js
@@ -1,8 +1,11 @@
 
 
 
-  var passLength = form ("How many characters do you want in your password?")
-  var lowercase = prompt ("Do you want to include lowercase letters?")
-  var uppercase = prompt ("Do you want to include uppercase letters?")
-  var numbers = prompt ("Do you want to include numbers?")
-  var special = prompt ("Do you want to include special characters?")
+  var passLength = form ("How many characters do you want in your password?");
+  var lowercase = prompt ("Do you want to include lowercase letters?");
+  var uppercase = prompt ("Do you want to include uppercase letters?");
+  var numbers = prompt ("Do you want to include numbers?");
+  var special = prompt ("Do you want to include special characters?");
+
+
+  function random
