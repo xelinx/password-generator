b555a5bdc9cc1944cd34a2e82cbdfc7153cee381
Mona <66236313+xelinx@users.noreply.github.com>
2020-06-28 21:53:10 -0700

    button

--- javascript.js
+++ javascript.js
@@ -2,14 +2,34 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var number = "0123456789";
 var specialChar = "!@#$%^&*/-+";
+var passwordCharSet;
 
   var passLength = form ("How many characters do you want in your password?");
-  var lowercase = prompt ("Do you want to include lowercase letters?");
-  var uppercase = prompt ("Do you want to include uppercase letters?");
-  var numbers = prompt ("Do you want to include numbers?");
-  var special = prompt ("Do you want to include special characters?");
+  var lowercase = confirm ("Do you want to include lowercase letters?");
+  var uppercase = confirm ("Do you want to include uppercase letters?");
+  var numbers = confirm ("Do you want to include numbers?");
+  var special = confirm ("Do you want to include special characters?");
 
 
   function generatePassword () {
-
-  }
+    userPassword = "";
+    passwordCharSet = "";
+    if (lowercaseInput.checked) {
+      passwordCharSet += lowercase;
+    }
+    if (uppercaseInput.checked) {
+      passwordCharSet += uppercase;
+    }
+    if (punctuationInput.checked) {
+      passwordCharSet += punctuation;
+    }
+    if (numbersInput.checked) {
+      passwordCharSet += numbers;
+    }
+    plength = Number(lengthInput.value);
+   
+    for (let i = 0; i < plength; i++) {
+      userPassword += passwordCharSet.charAt(
+        Math.floor(Math.random() * passwordCharSet.length)
+      );
+    }  
