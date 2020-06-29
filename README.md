0bbe3f51c053a6d4989a7ac5e45f631973922454
Mona <66236313+xelinx@users.noreply.github.com>
2020-06-29 04:03:20 -0700

    image

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 Creating an application that generates a random password (8-128 characters)at the click of a button. The password will be created based on the user's answer to prompts.
 
-This time around, I really struggled with the javascript. 
+This time around, I really struggled with the javascript. I know what I want my code to do, but struggled to lay it out via javascript. I look at the code and feel like it could be drier, but also at the same time, it seems like the code I have currently is not enough (and doesn't... work)
 
 ## Screenshots of the Deployed Website
+![Page](https://user-images.githubusercontent.com/66236313/85996692-6a346e80-b9bd-11ea-945c-c84baa0cd2bc.png)
 
 
 ## Review
--- script.js
+++ script.js
@@ -5,44 +5,43 @@ function promptLength () {
         if (passLength <8) {
             "Please enter a number between 8 and 128.";
         }
-    }
+    },
 addLower: function() {
     var lowercase = confirm ("Do you want to include lowercase letters?");
         if( lowercase === "true" ) {
             this.charSet= this.charSet + "abcdefghijklmnopqrstuvwxyz";
         }
-        else( lowercase === "false"){
+        else( lowercase === "false") {
             this.charSet;
         }
-    }
+    },
 addUpper: function() {
     var uppercase = confirm ("Do you want to include uppercase letters?");
         if( uppercase === "true" ) {
             this.charSet= this.charSet + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         } 
-        else( lowercase === "false"){
+        else( lowercase === "false") {
             this.charSet;
         }
-    }
+    },
 addNumber: function() {  
     var numbers = confirm ("Do you want to include numbers?");
         if( numbers === "true" ) {
             this.charSet = this.charSet + "0123456789";
         } 
-        else( lowercase === "false"){
+        else( lowercase === "false") {
             this.charSet;
         }
-    }
+    },
 addSpecial: function() {
     var special = confirm ("Do you want to include special characters?");
         if( special === "true" ) {
             this.charSet = this.charSet + "~!@#$%^&*/-+";
         } 
-        else( lowercase === "false"){
+        else( lowercase === "false") {
             this.charSet;
         }
-    }
-};
+    },
     var value = "";
     for (var i = 0; i < length; i++) {
       value += charSet.charAt(Math.floor(Math.random() * charSet.length));
