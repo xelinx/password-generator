a84caffabac8d06b4b9bda72b7d2e5b84ded7dc0
Mona <66236313+xelinx@users.noreply.github.com>
2020-06-29 03:45:15 -0700

    code

--- script.js
+++ script.js
@@ -1,41 +1,53 @@
 
-function generatePassword (plength) {
+function promptLength () {
     var charSet = "";
     var passLength = Number(prompt ("How many characters do you want your password to be? Enter a number between 8 and 128."));
         if (passLength <8) {
-            "Please enter a number between 8 and 128."
-        };
-        if else (passLength >128) {
-            "Please enter a number between 8 and 128."
-        };
-        else (passLength) {
-            
-        };
-
+            "Please enter a number between 8 and 128.";
+        }
+}
+addLower: function() {
     var lowercase = confirm ("Do you want to include lowercase letters?");
-    var uppercase = confirm ("Do you want to include uppercase letters?");
-    var numbers = confirm ("Do you want to include numbers?");
-    var special = confirm ("Do you want to include special characters?");
         if( lowercase === "true" ) {
-            charSet + "abcdefghijklmnopqrstuvwxyz";
+            this.charSet= this.charSet + "abcdefghijklmnopqrstuvwxyz";
+        }
+        else( lowercase === "false"){
+            this.charSet;
+        }
+}
+addUpper: function() {
+    var uppercase = confirm ("Do you want to include uppercase letters?");
+        if( uppercase === "true" ) {
+            this.charSet= this.charSet + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         } 
-        else if( uppercase === "true" ) {
-            charSet + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        else( lowercase === "false"){
+            this.charSet;
+        }
+}
+addNumber: function() {  
+    var numbers = confirm ("Do you want to include numbers?");
+        if( numbers === "true" ) {
+            this.charSet = this.charSet + "0123456789";
         } 
-        else if( numbers === "true" ) {
-            charSet + "0123456789";
+        else( lowercase === "false"){
+            this.charSet;
+        }
+}
+addSpecial: function() {
+    var special = confirm ("Do you want to include special characters?");
+        if( special === "true" ) {
+            this.charSet = this.charSet + "~!@#$%^&*/-+";
         } 
-        else if( special === "true" ) {
-            charSet + "~!@#$%^&*/-+";
-        } 
-    plength = Number(lengthInput.value);
+        else( lowercase === "false"){
+            this.charSet;
+        }
+}
+
 
     var value = "";
     for (var i = 0; i < length; i++) {
       userPassword += charSet.charAt(
-        value+ (Math.floor(Math.random() * charSet.length)
-      );
+        value+ (Math.floor(Math.random() * charSet.length));
     }
     return value
 }
-alert(generatePassword());
