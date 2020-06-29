84039ae45c04471ed3fc174d5a8b5f27bcb0b315
Mona <66236313+xelinx@users.noreply.github.com>
2020-06-28 23:56:44 -0700

    add

--- javascript.js
+++ javascript.js
@@ -3,24 +3,21 @@ var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var number = "0123456789";
 var specialChar = "!@#$%^&*/-+";
 var passwordCharSet;
-
-  var passLength = form ("How many characters do you want in your password?");
-  var lowercase = confirm ("Do you want to include lowercase letters?");
-  var uppercase = confirm ("Do you want to include uppercase letters?");
-  var numbers = confirm ("Do you want to include numbers?");
-  var special = confirm ("Do you want to include special characters?");
-
-
-    // Loop over every question object
-    for (var i = 0; i < questions.length; i++) {
-        // Display current question to user and ask OK/Cancel
-        var answer = confirm(questions[i].q);
-
-  function generatePassword () {
+var copyButton = document.getElementsById ("copy");
+function generate() {
     userPassword = "";
     passwordCharSet = "";
-    if (lowercase, uppercase, numbers, special === true) {
-      passwordCharSet += lowercase, uppercase, numbers, special;
+    if (lowercase ===trie) {
+      passwordCharSet += lowercase;
+    }
+    if (uppercase === true) {
+      passwordCharSet += uppercase;
+    }
+    if (number ===true) {
+      passwordCharSet += punctuation;
+    }
+    if (specialChar === true) {
+      passwordCharSet += numbers;
     }
     plength = Number(lengthInput.value);
    
@@ -28,6 +25,62 @@ var passwordCharSet;
       userPassword += passwordCharSet.charAt(
         Math.floor(Math.random() * passwordCharSet.length)
       );
-    }  
+    }
+    if (userPassword == "") {
+      let alertbox = document.getElementById('alert');
+      alertbox.innerHTML = "Please select an option before generating"
+      alertbox.classList.add('fail');
+      setTimeout(function(){ 
+        alertbox.classList.remove('fail');
+      }, 3000);
+    } else {
+      passwordFeild.innerHTML = userPassword;
+    }
+    copyButton.setAttribute("data-clipboard-text", userPassword)
+  }
+  generateButton.addEventListener("click", generate);
+ 
 
-generateButton.addEventListener("click", generate);
+        button.addEventListener("click", function()
+        {
+            input.value = generatePassword(length.value);
+        });
+
+
+
+        function generatePassword(n) 
+        {
+            let pwd = "";
+                
+            while(!pwd || pwd.length  n<=128)
+            {
+                pwd += Math.random().toString(36).slice(-22);
+            }
+                  
+            return pwd.substring(0, n);
+        }
+                
+        
+          function generatePassword () {
+            userPassword = "";
+            passwordCharSet = "";
+            if (lowercase, uppercase, numbers, special === true) {
+              passwordCharSet += lowercase, uppercase, numbers, special;
+            }
+            plength = Number(lengthInput.value);
+           
+            for (let i = 0; i < plength; i++) {
+              userPassword += passwordCharSet.charAt(
+                Math.floor(Math.random() * passwordCharSet.length)
+              );
+            }  
+        
+        function populateform(enterlength){
+            document.pgenerate.output.value=generatePassword(enterlength)
+        
+            var passLength = form ("How many characters do you want in your password?");
+            var lowercase = confirm ("Do you want to include lowercase letters?");
+            var uppercase = confirm ("Do you want to include uppercase letters?");
+            var numbers = confirm ("Do you want to include numbers?");
+            var special = confirm ("Do you want to include special characters?");
+          
