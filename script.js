e1be2bb015430a934231bce87c8f5708d057c7e0
Mona <66236313+xelinx@users.noreply.github.com>
2020-06-29 02:05:04 -0700

    add

--- script.js
+++ script.js
@@ -1,43 +1,41 @@
-var questions ["Do you want to include"+lowercase, uppercase, numbers, special"in your password?"];
 
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var number = "0123456789";
-var specialChar = "!@#$%^&*/-+";
-var userPassword;
-var passwordCharSet;
-var Button = document.getElementsById ("button");
+function promptLength () {
+    var passLength = Number(prompt ("How many characters do you want your password to be? Enter a number between 8 and 128."));
+        if (passLength <8, >128 ) {
+            "Please enter a number between 8 and 128."
+            promptLength ();
+        }
+    
+}
 
-
-var passLength = prompt ("How many characters do you want your password to be? Enter a number between 8 and 128.");
-    if (passLength <8) {
-        alert ("Please select between 8 and 128");
-    }
-    else if (passLength >128) {
-        alert ("Please select between 8 and 128");
-    }
-
+function characters () {
     var lowercase = confirm ("Do you want to include lowercase letters?");
     var uppercase = confirm ("Do you want to include uppercase letters?");
     var numbers = confirm ("Do you want to include numbers?");
     var special = confirm ("Do you want to include special characters?");
-    
-    function generate () {
-        userPassword = "";
-        passwordCharSet = "";
-        if (lowercase === true) {
-            passwordCharSet+= lowerCase;
-        }
-        if (uppercase === true) {
-            passwordCharSet+= upperCase;
-        }
-        if (numbers === true) {
-            passwordCharSet+= number;
-        }
-        if (special === true) {
-            passwordCharSet+= specialChar;
-        }
-        return 
-    }
+}
 
-button.addEventListener("click", generate);
+function generatePassword () {
+    userPassword = "";
+    passwordCharSet = "";
+    if (lowercase === true) {
+        passwordCharSet+ lowerCase;
+    }
+    if (uppercase === true) {
+        passwordCharSet+ upperCase;
+    }
+    if (numbers === true) {
+        passwordCharSet+ number;
+    }
+    if (special === true) {
+        passwordCharSet+ specialChar;
+    }
+    plength = Number(lengthInput.value);
+
+    for (let i = 0; i < plength; i++) {
+      userPassword += passwordCharSet.charAt(
+        Math.floor(Math.random() * passwordCharSet.length)
+      );
+    }
+    return 
+}
