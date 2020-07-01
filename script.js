2e20e20ed68932241a6d5668819cb549fa26bfef
Mona <66236313+xelinx@users.noreply.github.com>
2020-06-30 22:28:06 -0700

    more

--- script.js
+++ script.js
@@ -7,18 +7,27 @@ var number = "0123456789";
 var special = "!@#$%^&*/-+";
 
 var passLength;
+var lc;
 
 //Ask password Length
 function promptLength () {
+    var passLength = "";
     var passLength = Number(prompt ("How many characters do you want your password to be? Enter a number between 8 and 128."));
 
     //Loop if length isn't 8-128
-    while(promptLength <= 8 || promptLength >= 128) {
+    while(promptLength <= 8 && promptLength >= 128) {
         alert("Please enter a number between 8 and 128");
         var passLength = Number(prompt ("How many characters do you want your password to be? Enter a number between 8 and 128."));
     }
 
+    var lc = confirm ("Do you want to include lowercase letters?");
+    var uppercase = confirm ("Do you want to include uppercase letters?");
+    var numbers = confirm ("Do you want to include numbers?");
+    var special = confirm ("Do you want to include special characters?");
+
+
 };
+
 
 
 
