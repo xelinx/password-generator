0a4cc41bc73fc231ffd1c72fc36ef7bdb3258673
Mona <66236313+xelinx@users.noreply.github.com>
2020-06-30 19:14:58 -0700

    edit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.querySelector("#prompt").addEventListener("click", promptLength);
 
+//Variables
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var number = "0123456789";
@@ -7,52 +8,66 @@ var special = "!@#$%^&*/-+";
 
 var passLength;
 
-function promptLength () {
-    var passLength = Number(prompt ("How many characters do you want your password to be? Enter a number between 8 and 128."));
-}
-
-var password = {
 //Ask password Length
 function promptLength () {
-    var charSet = "";
     var passLength = Number(prompt ("How many characters do you want your password to be? Enter a number between 8 and 128."));
-        if (passLength <8) {
-            "Please enter a number between 8 and 128.";
-        }
-    },
-addLower: function() {
-    var lowercase = confirm ("Do you want to include lowercase letters?");
-        if( lowercase === "true" ) {
-            this.charSet= this.charSet + "abcdefghijklmnopqrstuvwxyz";
-        }
-        else( lowercase === "false") {
-            this.charSet;
-        }
-    },
-addUpper: function() {
-    var uppercase = confirm ("Do you want to include uppercase letters?");
-        if( uppercase === "true" ) {
-            this.charSet= this.charSet + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        } 
-        else( lowercase === "false") {
-            this.charSet;
-        }
-    },
-addNumber: function() {  
-    var numbers = confirm ("Do you want to include numbers?");
-        if( numbers === "true" ) {
-            this.charSet = this.charSet + "0123456789";
-        } 
-        else( lowercase === "false") {
-            this.charSet;
-        }
-    },
-addSpecial: function() {
-    var special = confirm ("Do you want to include special characters?");
-        if( special === "true" ) {
-            this.charSet = this.charSet + "~!@#$%^&*/-+";
-        } 
-        else( lowercase === "false") {
-            this.charSet;
-        }
-    },
+
+    //Loop if length isn't 8-128
+    while(promptLength <=)
+
+    if (passLength <8) {
+        "Please enter a number between 8 and 128.";
+    }
+    else if (passLength >128) {
+        "Please enter a number between 8 and 128.";
+    }
+
+};
+
+
+var password = {
+    //Ask password Length
+    function promptLength () {
+        var charSet = "";
+        var passLength = Number(prompt ("How many characters do you want your password to be? Enter a number between 8 and 128."));
+            if (passLength <8) {
+                "Please enter a number between 8 and 128.";
+            }
+        },
+    addLower: function() {
+        var lowercase = confirm ("Do you want to include lowercase letters?");
+            if( lowercase === "true" ) {
+                this.charSet= this.charSet + "abcdefghijklmnopqrstuvwxyz";
+            }
+            else( lowercase === "false") {
+                this.charSet;
+            }
+        },
+    addUpper: function() {
+        var uppercase = confirm ("Do you want to include uppercase letters?");
+            if( uppercase === "true" ) {
+                this.charSet= this.charSet + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            } 
+            else( lowercase === "false") {
+                this.charSet;
+            }
+        },
+    addNumber: function() {  
+        var numbers = confirm ("Do you want to include numbers?");
+            if( numbers === "true" ) {
+                this.charSet = this.charSet + "0123456789";
+            } 
+            else( lowercase === "false") {
+                this.charSet;
+            }
+        },
+    addSpecial: function() {
+        var special = confirm ("Do you want to include special characters?");
+            if( special === "true" ) {
+                this.charSet = this.charSet + "~!@#$%^&*/-+";
+            } 
+            else( lowercase === "false") {
+                this.charSet;
+            }
+        },
+    
