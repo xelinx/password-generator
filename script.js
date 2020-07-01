10191b21e643bc77df2a35a4e42ede9f8efde38b
Mona <66236313+xelinx@users.noreply.github.com>
2020-06-30 23:14:46 -0700

    add variable

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ var number = "0123456789";
 var special = "!@#$%^&*/-+";
 
 var passLength;
-var lc;
+var confirmLowercase;
+var confirmUppercase;
+var confirmNumbers;
+var confirmSpecial; 
+var characters;
+
 
 //Ask password Length
 function promptLength () {
@@ -21,60 +26,13 @@ function promptLength () {
     }
 
     //confirm inclusion of characters, numbers, special characers
-    var lc = confirm ("Do you want to include lowercase letters?");
+    var lowercase = confirm ("Do you want to include lowercase letters?");
     var uppercase = confirm ("Do you want to include uppercase letters?");
     var numbers = confirm ("Do you want to include numbers?");
     var special = confirm ("Do you want to include special characters?");
 
-
+    var characters = [lowercase, uppercase, numbers, special]
 };
 
 
 
-
-var password = {
-    //Ask password Length
-    function promptLength () {
-        var charSet = "";
-        var passLength = Number(prompt ("How many characters do you want your password to be? Enter a number between 8 and 128."));
-            if (passLength <8) {
-                "Please enter a number between 8 and 128.";
-            }
-        },
-    addLower: function() {
-        var lowercase = confirm ("Do you want to include lowercase letters?");
-            if( lowercase === "true" ) {
-                this.charSet= this.charSet + "abcdefghijklmnopqrstuvwxyz";
-            }
-            else( lowercase === "false") {
-                this.charSet;
-            }
-        },
-    addUpper: function() {
-        var uppercase = confirm ("Do you want to include uppercase letters?");
-            if( uppercase === "true" ) {
-                this.charSet= this.charSet + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            } 
-            else( lowercase === "false") {
-                this.charSet;
-            }
-        },
-    addNumber: function() {  
-        var numbers = confirm ("Do you want to include numbers?");
-            if( numbers === "true" ) {
-                this.charSet = this.charSet + "0123456789";
-            } 
-            else( lowercase === "false") {
-                this.charSet;
-            }
-        },
-    addSpecial: function() {
-        var special = confirm ("Do you want to include special characters?");
-            if( special === "true" ) {
-                this.charSet = this.charSet + "~!@#$%^&*/-+";
-            } 
-            else( lowercase === "false") {
-                this.charSet;
-            }
-        },
-    
