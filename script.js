64acf654d4c9f2834e24653e3964a2c937ef353a
Mona <66236313+xelinx@users.noreply.github.com>
2020-06-30 23:43:52 -0700

    added if loop

--- script.js
+++ script.js
@@ -20,10 +20,17 @@ function promptLength () {
     var passLength = Number(prompt ("How many characters do you want your password to be? Enter a number between 8 and 128."));
 
     //Loop if length isn't 8-128
-    while(promptLength <= 8 && promptLength >= 128) {
+    if (passLength <8) {
         alert("Please enter a number between 8 and 128");
         var passLength = Number(prompt ("How many characters do you want your password to be? Enter a number between 8 and 128."));
     }
+    else if (passLength >128) {
+        alert("Please enter a number between 8 and 128");
+        var passLength = Number(prompt ("How many characters do you want your password to be? Enter a number between 8 and 128."));
+    }
+
+    //Display password length
+    alert("Your password will be " + passLength + " characters");
 
     //confirm inclusion of characters, numbers, special characers
     var lowercase = confirm ("Do you want to include lowercase letters?");
